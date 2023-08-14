768a204879350648f83c45300f6979a4dd7e7893
alkynee <alkynee@proton.me>
2023-08-14 22:44:01 +0000

    Fix ruby version

--- cvgo/src/memory.h
+++ cvgo/src/memory.h
@@ -18,4 +18,5 @@ class memory {
 private:
 	bool setProcessHandle(const wchar_t* name);
 	bool setModuleInfo(const wchar_t* name);
-	unsigned long patternScan(const char* pattern, const 
+	unsigned long patternScan(const char* pattern, const char* mask, const unsigned long begin, const unsigned long size);
+p
