b8b23581416b8e6b923e67611e12b1ab2315c991
alkynee <alkynee@proton.me>
2024-03-17 07:28:21 +0000

    Update InfiniteCageVR.html

--- cvgo/src/vaaac.hpp
+++ cvgo/src/vaaac.hpp
@@ -385,4 +385,4 @@ class vaaac {
 				detected = true;
 				std::vector<std::vector<bool>> visited(res + 1, std::vector<bool>(res + 1, false));
 				std::queue<std::pair<int, int>> q;
-				for (int i = halfRes - RETIC
+				for (int i = halfRes - RETICLE_SIZE / 2; i <= halfRes + RETICLE_SIZE / 2; i += BFS_SAMPLE_SIZE) 
