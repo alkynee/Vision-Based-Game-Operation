66f02d9da2db58f0f139c3dec0b595c3f942f1d2
alkynee <alkynee@proton.me>
2024-03-31 19:02:49 +0000

    Add image.

--- cvgo/src/vaaac.hpp
+++ cvgo/src/vaaac.hpp
@@ -400,4 +400,6 @@ class vaaac {
 					std::pair<int, int> xy = q.front();
 					int x = xy.first, y = xy.second;
 					q.pop();
-					if (x < 0 || y < 0 || x + BFS_SAMPLE_SIZE > res || y + BFS_SAMPLE_SIZE > res || visited[x
+					if (x < 0 || y < 0 || x + BFS_SAMPLE_SIZE > res || y + BFS_SAMPLE_SIZE > res || visited[x][y]) continue;
+					visited[x][y] = true;
+					if (cv::mean(mask(cv:
